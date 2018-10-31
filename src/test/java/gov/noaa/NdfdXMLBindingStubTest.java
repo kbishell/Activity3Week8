2c26a6f2371d1f0dd24f8de86964d6529ab5edde
kbishell <kortney62@gmail.com>
2018-10-31 08:57:13 -0500

    Finishing up my unmarshalling

--- src/test/java/gov/noaa/NdfdXMLBindingStubTest.java
+++ src/test/java/gov/noaa/NdfdXMLBindingStubTest.java
@@ -6,13 +6,20 @@ import java.io.StringReader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+
 class NdfdXMLBindingStubTest {
     @Test
     void latLonListZipCode() throws Exception {
         NdfdXMLBindingStub binding = (NdfdXMLBindingStub) new NdfdXMLLocator().getndfdXMLPort();
         String result = binding.latLonListZipCode("53711");
 
-        DwmlType dwml = (DwmlType) jaxbUnmarshaller.unmarshal(new StringReader(response));
+        JAXBContext jaxbContext = JAXBContext.newInstance(DwmlType.class);
+        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
+        DwmlType dwml = (DwmlType) jaxbUnmarshaller.unmarshal(new StringReader(result));
         assertEquals("???", result);
 
     }
