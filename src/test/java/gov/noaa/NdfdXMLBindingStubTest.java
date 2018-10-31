e2f39dede98542fabace73105db5e09c9a992560
kbishell <kortney62@gmail.com>
2018-10-31 09:03:04 -0500

    Finishing up my unmarshalling

--- src/test/java/gov/noaa/NdfdXMLBindingStubTest.java
+++ src/test/java/gov/noaa/NdfdXMLBindingStubTest.java
@@ -2,13 +2,14 @@ package gov.noaa;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.File;
 import java.io.StringReader;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
-import javax.xml.bind.Marshaller;
+import javax.xml.bind.Unmarshaller;
 
 
 class NdfdXMLBindingStubTest {
@@ -17,9 +18,16 @@ class NdfdXMLBindingStubTest {
         NdfdXMLBindingStub binding = (NdfdXMLBindingStub) new NdfdXMLLocator().getndfdXMLPort();
         String result = binding.latLonListZipCode("53711");
 
+        // create JAXB context and initializing Marshaller
         JAXBContext jaxbContext = JAXBContext.newInstance(DwmlType.class);
-        Marshaller jaxbMarshaller = jaxbContext.createMarshaller();
-        DwmlType dwml = (DwmlType) jaxbUnmarshaller.unmarshal(new StringReader(result));
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+
+        // specify the location and name of xml file to be read
+        File XMLfile = new File('C:\\arpit\\CountryRecord.xml');
+
+        // this will create Java object - country from the XML file
+        Country countryIndia = (Country) jaxbUnmarshaller.unmarshal(XMLfile);
+
         assertEquals("???", result);
 
     }
